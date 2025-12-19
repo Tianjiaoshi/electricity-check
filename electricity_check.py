7e15f896a76f92fc7ebbe124f9adc7f0e3d00f56
Tianjiaoshi <72741982+Tianjiaoshi@users.noreply.github.com>
2025-12-19 21:57:31 +0800

    Update environment variable loading in electricity_check.py
    Remove default values for environment variables.

--- electricity_check.py
+++ electricity_check.py
@@ -8,8 +8,8 @@ import re
 
 # --- 1. 配置区域---
 # 从环境变量（GitHub Secrets）中加载所有敏感信息
-WECHAT_WORK_WEBHOOK = os.environ.get('WECHAT_WORK_WEBHOOK', 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=98b7363a-4829-4bd3-9452-cc40a6c4415f')
-JSESSIONID = os.environ.get('JSESSIONID', '27C908135783AB1DBF6CEFC8E2A982FF')
+WECHAT_WORK_WEBHOOK = os.environ.get('WECHAT_WORK_WEBHOOK')
+JSESSIONID = os.environ.get('JSESSIONID')
 
 # 检查所有必需的密钥（Secrets）是否已成功加载
 if not all([WECHAT_WORK_WEBHOOK, JSESSIONID]):
